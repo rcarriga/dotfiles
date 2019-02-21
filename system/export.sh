b086c68c574859def241bf29e3eab3fd85be2489
ronanc <ronanc@qualtrics.com>
2019-02-21 21:27:46 +0000

    Removed java home

--- system/export.sh
+++ system/export.sh
@@ -4,6 +4,5 @@ export ZSH=~/".oh-my-zsh"
 export TERM=xterm-256color
 export MANPATH="/usr/local/man:$MANPATH"
 export EDITOR='vim'
-export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
 export M2_HOME=/Applications/apache-maven-3.6.0
 export PATH=$PATH:$M2_HOME/bin
