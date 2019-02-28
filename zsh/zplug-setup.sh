48a42b0c2d4b605f24cdecb4b390a56304d381ac
ronanc <ronanc@qualtrics.com>
2019-02-28 14:15:21 +0000

    Switched back to pure for zsh.

--- zsh/zplug-setup.sh
+++ zsh/zplug-setup.sh
@@ -12,8 +12,10 @@ zplug "zsh-users/zsh-syntax-highlighting", from:github, defer:1
 zplug "plugins/autojump", from:oh-my-zsh
 zplug "supercrabtree/k", from:github
 zplug "plugins/git",   from:oh-my-zsh
-zplug "denysdovhan/spaceship-prompt", use:spaceship.zsh, from:github, as:theme
+#zplug "denysdovhan/spaceship-prompt", use:spaceship.zsh, from:github, as:theme
+zplug "sindresorhus/pure", use:pure.zsh, from:github, as:theme
 zplug "chrissicool/zsh-256color", from:github
+zplug "mafredri/zsh-async", from:"github", use:"async.zsh"
 
 if ! zplug check --verbose; then
     printf "Install? [y/N]: "
