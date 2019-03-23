1df2bdc08eae774e00188d185e069fc8ec039e66
ronanc <ronanc@qualtrics.com>
2019-03-23 17:23:54 +0000

    Updated i3 and added i3blocks with rough scripts

--- system/export.sh
+++ system/export.sh
@@ -6,4 +6,5 @@ export EDITOR='vim'
 export PATH=$PATH:$M2_HOME/bin
 export FZF_DEFAULT_COMMAND='ag --depth 10 --hidden --ignore .git -f -g ""'
 export PATH=$HOME/.local/bin:$PATH
+export SCRIPT_DIR=$HOME/dotfiles/scripts
 [[ -f ~/dotfiles/system/local.export.sh ]] && source ~/dotfiles/system/local.export.sh
