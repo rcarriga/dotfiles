a57962db2c08fdda28891017601542066e4f95a8
ronanc <ronanc@qualtrics.com>
2019-04-03 13:30:34 +0100

    New git aliases

--- .config/system/alias.sh
+++ .config/system/alias.sh
@@ -9,4 +9,7 @@ alias tmux="TERM=screen-256color-bce tmux"
 alias icat="kitty +kitten icat"
 alias h='k -h'
 alias weather="curl http://wttr.in/"
-alias gg="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)%aD%C(reset) %C(white)%s%C(reset) %C(dim white)- %an <%ae> %C(reset) %C(auto)%d%C(reset)'"
+alias gg="git log --graph --abbrev-commit --branches --decorate --format=format:'%C(bold blue)%h%C(reset) %x09%C(bold white)%<(70,trunc)%s%C(reset) %C(magenta)- %an <%ae> %C(reset) %C(cyan)%ai%C(reset) %C(auto)%+d%C(reset)'"
+alias gs="git status"
+alias gp="git push"
+alias ga="git add"
