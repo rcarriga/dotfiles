578ab1c7986b8d78848fa1ab845b25c34f8a4279
ronanc <ronanc@qualtrics.com>
2019-04-03 17:15:12 +0100

    Changed git log alias

--- .config/system/alias.sh
+++ .config/system/alias.sh
@@ -10,6 +10,7 @@ alias icat="kitty +kitten icat"
 alias h='k -h'
 alias weather="curl http://wttr.in/"
 alias gg="git log --graph --abbrev-commit --branches --decorate --format=format:'%C(bold blue)%h%C(reset) %x09%C(bold white)%<(70,trunc)%s%C(reset) %C(magenta)- %an <%ae> %C(reset) %C(cyan)%ai%C(reset) %C(auto)%+d%C(reset)'"
+alias yg="yadm log --graph --abbrev-commit --branches --decorate --format=format:'%C(bold blue)%h%C(reset) %x09%C(bold white)%<(70,trunc)%s%C(reset) %C(magenta)- %an <%ae> %C(reset) %C(cyan)%ai%C(reset) %C(auto)%+d%C(reset)'"
 alias gs="git status"
 alias gp="git push"
 alias ga="git add"
