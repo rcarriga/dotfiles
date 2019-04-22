71337b8dcbe0a41f6c448d749885025a14d8b6f6
rcarriga <rcarriga@tcd.ie>
2019-04-22 18:51:21 +0100

    Major overhaul of XMonad. Also nicer wrapping in neovim

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,29 +1,30 @@
-{-# LANGUAGE FlexibleContexts #-}
-import           XMonad
-import           XMonad.Hooks.SetWMName
-import           XMonad.Actions.CycleRecentWS
-import           Data.Monoid
-import           System.Exit
-import           XMonad.Hooks.DynamicLog
-import qualified Data.Map                      as M
-import           XMonad.Util.EZConfig
-import           XMonad.Hooks.ManageDocks
-import           XMonad.Actions.WorkspaceNames
-import           XMonad.Util.Run
-import           XMonad.Layout.Tabbed
-import           XMonad.Layout.Grid
-import           XMonad.Layout.NoBorders
-import           XMonad.Util.SpawnOnce
-import           XMonad.Layout.Accordion
-import           XMonad.Layout.AutoMaster
-import           XMonad.Util.NamedScratchpad
-
-myScratchpads = [NS "htop" "kitty htop" (title =? "htop") defaultFloating]
-myTerminal = "kitty"
-myWorkspaces = map show [1 .. 9]
-myNormalBorderColor = "#333333"
-myFocusedBorderColor = "#888888"
-myModMask = mod4Mask
+{-# LANGUAGE UnicodeSyntax, FlexibleContexts, OverloadedStrings #-}
+import XMonad hiding (WindowClass)
+import XMonad.Config.Prime (WindowSpace)
+import XMonad.Hooks.SetWMName
+import XMonad.Actions.CycleRecentWS
+import Data.Monoid
+import System.Exit
+import XMonad.Hooks.DynamicLog
+import qualified Data.Map as M
+import XMonad.Util.EZConfig
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run
+import XMonad.Layout.Tabbed
+import XMonad.Layout.Grid
+import XMonad.Layout.NoBorders
+import XMonad.Util.SpawnOnce
+import XMonad.Layout.Accordion
+import XMonad.Layout.AutoMaster
+import XMonad.Util.NamedScratchpad
+import XMonad.Prompt.ConfirmPrompt
+import XMonad.Prompt
+import GHC.IO.Handle.Types
+import qualified XMonad.StackSet as W
+import Graphics.X11.Xlib.Extras (getClassHint, resClass)
+import Data.Foldable
+import Data.Maybe
+import Util
 
 startScript :: String -> X ()
 startScript script_name = spawn $ "bash $HOME/.config/scripts/" ++ script_name
@@ -32,35 +33,107 @@ main :: IO ()
 main = spawnPipe "xmobar" >>= xmonad . docks . myConfig
 
 myConfig pipe =
-    def { terminal           = myTerminal
-        , modMask            = myModMask
-        , workspaces         = myWorkspaces
-        , normalBorderColor  = myNormalBorderColor
-        , focusedBorderColor = myFocusedBorderColor
-        , manageHook         = manageDocks <+> def manageHook
-        , layoutHook         = myLayoutHook
-        , handleEventHook    = docksEventHook <+> def handleEventHook
-        , startupHook        = myStartupHook
-        , logHook            = myLogHook pipe
-        , borderWidth        = 1
-        }
+    def
+            { terminal           = myTerminal
+            , modMask            = myModMask
+            , workspaces         = myWorkspaces
+            , normalBorderColor  = myNormalBorderColor
+            , focusedBorderColor = myFocusedBorderColor
+            , manageHook         = myManageHook
+            , layoutHook         = myLayoutHook
+            , handleEventHook    = docksEventHook <+> def handleEventHook
+            , startupHook        = myStartupHook
+            , logHook            = myLogHook pipe
+            , borderWidth        = 2
+            }
         `additionalKeysP` myKeys
 
-myLogHook pipe = dynamicLogWithPP xmobarPP { ppOutput = hPutStrLn pipe }
+myScratchpads :: [NamedScratchpad]
+myScratchpads =
+    [ NS "htop"            "kitty htop"      (title =? "htop")                defaultFloating
+    , NS "blueman-manager" "blueman-manager" (className =? "blueman-manager") defaultFloating
+    ]
 
-myLayoutHook =
-    avoidStruts
-        $   Tall 1 (3 / 100) (1 / 2)
-        ||| noBorders Full
-        ||| Accordion
-        ||| autoMaster 1 (1 / 100) Grid
+myTerminal :: String
+myTerminal = "kitty"
+
+myWorkspaces :: [String]
+myWorkspaces = map show [1 .. 9]
+
+myNormalBorderColor :: String
+myNormalBorderColor = "#333333"
+
+myFocusedBorderColor :: String
+myFocusedBorderColor = "#888888"
+
+myModMask :: KeyMask
+myModMask = mod4Mask
+
+myManageHook :: ManageHook
+myManageHook = composeAll [manageDocks, def manageHook]
+
+myLogHook :: Handle -> X ()
+myLogHook pipe = do
+    m <- buildIconMap
+    dynamicLogWithPP def
+        { ppOutput  = hPutStrLn pipe . pad
+        , ppCurrent = xmobarColor "white" "black" . parseWorkspaceId m
+        , ppHidden  = xmobarColor "#555555" "black" . parseWorkspaceId m
+        , ppSep     = " | "
+        , ppWsSep   = xmobarColor "#555555" "black" "  "
+        , ppLayout  = const ""
+        , ppTitle   = const ""
+        }
+
+
+-- ######################################################################################
+-- This section is used to convert a workspace ID to a string containing an icon for the focused window in that workspace
+-- Can be used for XMobar (Or any status bar desired)
+--
+
+-- | Given the mappings of workspace IDs to their strings aliases
+-- an ID return a string to represent workspace in XMobar.
+-- Also returns empty string for NamedScratchpad workspace.
+parseWorkspaceId :: WorkspaceAliases -> WorkspaceId -> String
+parseWorkspaceId m i = if i == "NSP" then "" else fromMaybe i $ M.lookup i m
+
+type WindowClass = String
+type WindowIcons = M.Map WindowClass String
+
+-- | Icons to represent a specified window class
+windowIcons :: WindowIcons
+windowIcons =
+    let wrapIcon icon = " <fn=1>" <> icon <> "</fn>"
+    in M.fromList $ map (liftSnd wrapIcon) [("kitty", "\xf120"), ("Firefox", "\xf269"), ("Blueman-manager", "\xf294")]
+
+type WorkspaceAliases = M.Map WorkspaceId String
+
+-- | Cycle through workspaces and store workspace aliases
+buildIconMap :: X WorkspaceAliases
+buildIconMap = foldrM storeAlias M.empty =<< W.workspaces . windowset <$> get
+
+-- | Store the given workspace's name in given and return.
+-- This is where to change if the desired format is not <Workspace tag><Window Icon>
+storeAlias :: W.Workspace WorkspaceId l Window -> WorkspaceAliases -> X WorkspaceAliases
+storeAlias ws m = do
+    dis <- display <$> ask
+    case W.stack ws of
+        Nothing -> return m
+        Just a  -> do
+            focus <- liftIO $ resClass <$> getClassHint dis (W.focus a)          -- Get focused window in given workspace
+            let wsName = W.tag ws <> fromMaybe "" (M.lookup focus windowIcons)   -- Create name to give workspace (i.e. Append icon to tag)
+            return $ M.insert (W.tag ws) wsName m                                -- Store name in Map
+
+-- ######################################################################################
+
+myLayoutHook = avoidStruts $ Tall 1 (3 / 100) (1 / 2) ||| noBorders Full
 
 myStartupHook :: X ()
 myStartupHook = do
     setWMName "XMonad"
     mapM_
         spawn
-        [ "pkill trayer; trayer --expand true --transparent true --margin 5 --iconspacing 5 --edge top --align right --width 2 --height 19 --tint 0x000000 --SetPartialStrut true --padding 5"
+        [ "pkill trayer; trayer --expand true --transparent true --margin 5 --iconspacing 5 --edge bottom --align right --width 2 --height 20 --tint 0x000000 --SetDockType true --SetPartialStrut true --padding 5"
         , "pgrep redshift || redshift -l 53:-6 -t 6500:2500"
         , "pgrep nm-applet || nm-applet"
         , "pgrep blueman-applet || blueman-applet"
@@ -82,8 +155,13 @@ myKeys =
     , ("<XF86AudioLowerVolume>" , startScript "volume DOWN")
     , ("<XF86AudioMute>"        , startScript "volume MUTE")
     , ("M-p"                    , spawn "rofi -show run -opacity \"85\" ")
-    , ("M-<Tab>", cycleRecentWS [xK_Super_L] xK_Tab xK_BackSpace)
+    , ("M-m"                    , namedScratchpadAction myScratchpads "blueman-manager")
+    , ("M-<Tab>"                , cycleRecentWS [xK_Super_L] xK_Tab xK_BackSpace)
     , ("M-t"                    , sendMessage ToggleStruts)
-    , ("M-h"                    , namedScratchpadAction myScratchpads "htop")
+    , ("M-n"                    , namedScratchpadAction myScratchpads "htop")
+    , ("M-c"                    , confirmPrompt myPromptConfig "close window?" kill)
+    , ("M-S-q"                  , confirmPrompt myPromptConfig "exit" $ io exitSuccess)
     ]
 
+myPromptConfig :: XPConfig
+myPromptConfig = def { font = "FiraCode-Regular", bgColor = "black", fgColor = "white", height = 25 }
--- .zshrc
+++ .zshrc
@@ -32,30 +32,30 @@ zplug load
 [[ -s /home/ronan/.autojump/etc/profile.d/autojump.sh ]] && source /home/ronan/.autojump/etc/profile.d/autojump.sh
 
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
-[ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"  # This loads nvm bash_completion
+#export NVM_DIR="$HOME/.nvm"
+#[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+#[ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"  # This loads nvm bash_completion
 
-autoload -U add-zsh-hook
-load-nvmrc() {
-  local node_version="$(nvm version)"
-  local nvmrc_path="$(nvm_find_nvmrc)"
+#autoload -U add-zsh-hook
+#load-nvmrc() {
+  #local node_version="$(nvm version)"
+  #local nvmrc_path="$(nvm_find_nvmrc)"
 
-  if [ -n "$nvmrc_path" ]; then
-    local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
+  #if [ -n "$nvmrc_path" ]; then
+    #local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
 
-    if [ "$nvmrc_node_version" = "N/A" ]; then
-      nvm install
-    elif [ "$nvmrc_node_version" != "$node_version" ]; then
-      nvm use
-    fi
-  elif [ "$node_version" != "$(nvm version default)" ]; then
-    echo "Reverting to nvm default version"
-    nvm use default
-  fi
-}
-add-zsh-hook chpwd load-nvmrc
-load-nvmrc
+    #if [ "$nvmrc_node_version" = "N/A" ]; then
+      #nvm install
+    #elif [ "$nvmrc_node_version" != "$node_version" ]; then
+      #nvm use
+    #fi
+  #elif [ "$node_version" != "$(nvm version default)" ]; then
+    #echo "Reverting to nvm default version"
+    #nvm use default
+  #fi
+#}
+#add-zsh-hook chpwd load-nvmrc
+#load-nvmrc
 
 ###-tns-completion-start-###
 if [ -f /Users/rcarriga/.tnsrc ]; then 
