11191c6e5789128dba313f26981d631e67ae3c02
ronanc <ronanc@qualtrics.com>
2019-05-09 09:33:30 +0100

    Add snippets and eslint

--- .config/system/alias.sh
+++ .config/system/alias.sh
@@ -3,7 +3,7 @@ alias python=python3.7
 alias tmux="TERM=screen-256color-bce tmux"
 alias icat="kitty +kitten icat"
 alias weather="curl http://wttr.in/"
-alias gg="git log --graph --abbrev-commit --branches --decorate --format=format:'%C(bold blue)%h%C(reset) %x09%C(bold white)%<(70,trunc)%s%C(reset) %C(magenta)- %an %C(reset) %C(cyan)%ai%C(reset) %C(auto)%+d%C(reset)'"
+alias gg="git log --graph --abbrev-commit --branches --decorate --format=format:'%C(bold blue)%h%C(reset) %x09%C(bold white)%<(70,trunc)%s%C(reset) %C(magenta)- %an %C(reset) %C(cyan)%ai%C(reset) %C(auto)%d%C(reset)'"
 alias yg="yadm log --graph --abbrev-commit --branches --decorate --format=format:'%C(bold blue)%h%C(reset) %x09%C(bold white)%<(70,trunc)%s%C(reset) %C(magenta)- %an %C(reset) %C(cyan)%ai%C(reset) %C(auto)%+d%C(reset)'"
 alias gs="git status"
 alias gp="git push"
@@ -11,4 +11,5 @@ alias ga="git add"
 alias gc="git commit"
 alias gco="git checkout"
 alias v=nvim
-[[ -f "~/.config/system/local.alias.sh" ]] && source ~/.config/system/local.alias.sh
+alias d="kitty +kitten diff"
+[[ -f "$HOME/.config/system/local.alias.sh" ]] && source "$HOME/.config/system/local.alias.sh"
