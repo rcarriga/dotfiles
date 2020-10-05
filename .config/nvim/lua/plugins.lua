aca6e221efc403369ac0fe0f38931cdcea283923
Rónán Carrigan <rcarriga@tcd.ie>
2020-10-05 08:42:52 +0100

    Lua plugins config

---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,14 @@
+require'colorizer'.setup({
+  vim = {names = false};
+}, {
+  RGB      = true;         -- #RGB hex codes
+  RRGGBB   = true;         -- #RRGGBB hex codes
+  names    = true;         -- "Name" codes like Blue
+  RRGGBBAA = false;        -- #RRGGBBAA hex codes
+  rgb_fn   = false;        -- CSS rgb() and rgba() functions
+  hsl_fn   = false;        -- CSS hsl() and hsla() functions
+  css      = false;        -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+  css_fn   = false;        -- Enable all CSS *functions*: rgb_fn, hsl_fn
+  -- Available modes: foreground, background
+  mode     = 'background'; -- Set the display mode.
+})
